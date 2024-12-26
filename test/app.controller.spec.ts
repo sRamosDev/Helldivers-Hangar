ae4971709c6226c5cba6a118479af8b1fe84ce3e
Sebastian Ramos <seba.ragu98@gmail.com>
2024-12-26 05:44:29 -0300

    re-enable tests

--- test/app.controller.spec.ts
+++ test/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '../src/app.controller';
+import { AppService } from '../src/app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return the routes', () => {
+      expect(appController.getRoutes()).toEqual({
+        endpoints: expect.any(Array),
+      });
     });
   });
 });
